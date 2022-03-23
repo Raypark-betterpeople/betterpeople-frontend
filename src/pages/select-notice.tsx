e10d18af7da433719aac28d2e1fbc0c321108331
Raypark-betterpeople <raypark@better-people.org>
2022-03-23 20:07:20 +0900

    select-notice loading

--- src/pages/select-notice.tsx
+++ src/pages/select-notice.tsx
@@ -7,6 +7,7 @@ import { isLoggedInVar } from "../apollo";
 import { CommonBodyContainer, Font } from "../common/styled";
 import { useMe } from "../hooks/useMe";
 import { notice, noticeVariables } from "../__generated__/notice";
+import { Loading } from "./loading";
 import { LoginHeader } from "./login-header";
 import { LogoutHeader } from "./logout-header";
 
@@ -61,20 +62,20 @@ export const SelectNotice = () => {
     },
   });
   return (
-    <CommonBodyContainer>
-      <Helmet>
-        <title>{`더 좋은 사람들 | ${data?.notice.notice?.mainTitle}`}</title>
-      </Helmet>
-      {isLoggedIn ? <LoginHeader /> : <LogoutHeader />}
-      <NoticeBox>
-      <Font fontWeight='700' fontColor='black' fontSize='1.8rem'>{data?.notice.notice?.mainTitle}</Font>
-      <Font fontWeight='400' fontColor='rgb(80,80,80)' fontSize='1rem'>{new Date(data?.notice.notice?.createAt).toLocaleString('ko')}</Font>
-      <NoticeBody>
-        <Image src={data?.notice.notice?.image} alt='공지사항 이미지'/>
-        <Font fontWeight='700' fontColor='black' fontSize='1.5rem' marginBottom='1rem'>{data?.notice.notice?.subTitle}</Font>
-        <Font fontWeight='400' fontColor='black' fontSize='1rem'>{data?.notice.notice?.description}</Font>
-      </NoticeBody>
-      </NoticeBox>
-    </CommonBodyContainer>
+    data?.notice ? <CommonBodyContainer>
+    <Helmet>
+      <title>{`더 좋은 사람들 | ${data?.notice.notice?.mainTitle}`}</title>
+    </Helmet>
+    {isLoggedIn ? <LoginHeader /> : <LogoutHeader />}
+    <NoticeBox>
+    <Font fontWeight='700' fontColor='black' fontSize='1.8rem'>{data?.notice.notice?.mainTitle}</Font>
+    <Font fontWeight='400' fontColor='rgb(80,80,80)' fontSize='1rem'>{new Date(data?.notice.notice?.createAt).toLocaleString('ko')}</Font>
+    <NoticeBody>
+      <Image src={data?.notice.notice?.image} alt='공지사항 이미지'/>
+      <Font fontWeight='700' fontColor='black' fontSize='1.5rem' marginBottom='1rem'>{data?.notice.notice?.subTitle}</Font>
+      <Font fontWeight='400' fontColor='black' fontSize='1rem'>{data?.notice.notice?.description}</Font>
+    </NoticeBody>
+    </NoticeBox>
+  </CommonBodyContainer> : <Loading />
   );
 };
